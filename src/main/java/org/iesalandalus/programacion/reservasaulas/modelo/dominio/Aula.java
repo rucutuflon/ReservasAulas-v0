5b0507e2023fbb8475f79d842a67c7edad962e4a
rucut <rucut@DESKTOP-NBA2QKJ>
2019-01-30 18:42:49 +0100

    Commit de la clase Aula

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/Aula.java
@@ -0,0 +1,64 @@
+package org.iesalandalus.programacion.reservasaulas.modelo.dominio;
+
+public class Aula {
+	
+	private String nombre;
+	
+	public Aula(String nombre) {
+		setNombre(nombre);
+	}
+	
+	public Aula(Aula a) {
+		if(a == null) {
+			throw new IllegalArgumentException("No se puede copiar un aula nula.");
+		}
+		setNombre(a.nombre);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		if(nombre == null) {
+			throw new IllegalArgumentException("El nombre del aula no puede ser nulo.");
+		}
+		if(nombre.isEmpty()) {
+			throw new IllegalArgumentException("El nombre del aula no puede estar vacío.");
+		}
+		this.nombre = nombre;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aula other = (Aula) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "[nombre=" + nombre + "]";
+	}
+	
+	
+
+}
