824d7d47d33dea5b054b6625a4de9f01b48b33a2
rucut <rucut@DESKTOP-NBA2QKJ>
2019-02-07 19:08:05 +0100

    Commit de la clase MainApp

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,9 +1,22 @@
 package org.iesalandalus.programacion.reservasaulas;
 
+import org.iesalandalus.programacion.reservasaulas.vista.Consola;
+import org.iesalandalus.programacion.reservasaulas.vista.Opcion;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Programa para la gesti√≥n de reservas de espacios del IES Al-√Åndalus");
+		System.out.println("Programa para la gestiÛn de reservas de espacios del IES Al-¡ndalus");
+		int opcion = 0;
+		
+		do {
+			Consola.mostrarCabecera("==========IES Al-¡ndalus==========");
+			Consola.mostrarMenu();
+			opcion = Consola.elegirOpcion();
+			Opcion.getOpcionSegunOrdinal(opcion).ejecutar();
+		} while (opcion != 0);
+	
 	}
 
 }
+
